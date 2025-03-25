7e9283b755a9c08f7f9c440675ba074861df0064
Jonnadaakshitha <245122748052@mvsrec.edu.in>
2025-03-25 12:00:04 +0530

    Add files via upload

---
+++ AppletDemo.java
@@ -0,0 +1,11 @@
+import java.applet.*;
+import java.awt.*;
+/*<applet code="AppletDemo.class" width="300" height="300">
+</applet>*/
+public class AppletDemo extends Applet
+{
+public void paint(Graphics g)
+{
+g.drawString("Hello World",100,150);
+}
+}
