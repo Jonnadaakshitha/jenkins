fa42e727eb3f81b8ff03adbf90068858995eb7e3
Jonnadaakshitha <245122748052@mvsrec.edu.in>
2025-03-25 12:34:51 +0530

    Applet

--- AppletDemo.java
+++ AppletDemo.java
@@ -7,5 +7,9 @@ public class AppletDemo extends Applet
 public void paint(Graphics g)
 {
 g.drawString("Hello World",100,150);
+g.drawLine(150,300,250,200);
+g.drawLine(175,300,170,500);
+g.drawRect(150,300,250,200);
+g.drawRect(300,359,200,200);
 }
 }
